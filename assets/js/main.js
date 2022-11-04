69f55e43fc01a0f5b65ddb6d0c5d1416f02f77b0
Daniel <angelicodaniel93@gmail.com>
2022-11-04 16:10:55 +0100

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,32 +3,32 @@ const team = [
     {
       name: 'Wayne Barnett',
       role: 'Founder & CEO',
-      image: 'assets/img/wayne-barnett-founder-ceo.jpg',
+      image: 'wayne-barnett-founder-ceo.jpg',
     },
     {
       name: 'Angela Caroll',
       role: 'Chief Editor',
-      image: 'assets/img/angela-caroll-chief-editor.jpg',
+      image: 'angela-caroll-chief-editor.jpg',
     },
     {
       name: 'Walter Gordon',
       role: 'Office Manager',
-      image: 'assets/img/walter-gordon-office-manager.jpg',
+      image: 'walter-gordon-office-manager.jpg',
     },
     {
       name: 'Angela Lopez',
       role: 'Social Media Manager',
-      image: 'assets/img/angela-lopez-social-media-manager.jpg',
+      image: 'angela-lopez-social-media-manager.jpg',
     },
     {
       name: 'Scott Estrada',
       role: 'Developer',
-      image: 'assets/img/scott-estrada-developer.jpg',
+      image: 'scott-estrada-developer.jpg',
     },
     {
       name: 'Barbara Ramos',
       role: 'Graphic Designer',
-      image: 'assets/img/barbara-ramos-graphic-designer.jpg',
+      image: 'barbara-ramos-graphic-designer.jpg',
     },
   ];
 
@@ -38,12 +38,11 @@ for (let i = 0; i < team.length; i++) {
     console.log(team[i])
     let currentElement = team[i]
     cards.innerHTML += `
-    <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="m-3" style="width: 20rem;">
+    <img class="card-img-top" src="assets/img/${currentElement.image}" alt="Card image cap">
     <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">${currentElement.name}</h5>
+    <p class="card-text">${currentElement.role}</p>
     </div>
     </div>`
 }
